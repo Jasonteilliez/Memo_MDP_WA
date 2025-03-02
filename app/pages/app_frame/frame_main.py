2e58831a747b860597b60f245584f15c47f0344a
Jason Teilliez <jason.teilliez@gmail.com>
2025-03-02 22:32:47 +0100

    update on_tab_change

--- app/pages/app_frame/frame_main.py
+++ app/pages/app_frame/frame_main.py
@@ -12,27 +12,30 @@ class FrameMain(tk.Frame):
 
 
     def init_ui(self, master):
-        notebook = ttk.Notebook(self)
-        notebook.pack(expand=True, fill="both")
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(expand=True, fill="both")
 
-        # Create frames for tabs
-        tab_motdepasse = ttk.Frame(notebook)
-        tab_categorie = ttk.Frame(notebook)
+        self.tab_motdepasse = ttk.Frame(self.notebook)
+        self.tab_categorie = ttk.Frame(self.notebook)
 
-        # Add tabs to the Notebook
-        notebook.add(tab_motdepasse, text="Mot de passe")
-        notebook.add(tab_categorie, text="Catégorie")
+        self.notebook.add(self.tab_motdepasse, text="Mot de passe")
+        self.notebook.add(self.tab_categorie, text="Catégorie")
 
-        self.frame_motdepasse = FrameMotdepasse(tab_motdepasse, master)
+        self.frame_motdepasse = FrameMotdepasse(self.tab_motdepasse, master)
         self.frame_motdepasse.pack(expand=True, fill='both', padx=10, pady=10)
 
-        self.frame_categorie = FrameCategorie(tab_categorie, master)
+        self.frame_categorie = FrameCategorie(self.tab_categorie, master)
         self.frame_categorie.pack(expand=True, fill='both', padx=10, pady=10)
 
-        notebook.bind("<<NotebookTabChanged>>", self.on_tab_change)
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_change)
 
     def on_tab_change(self, event):
-        self.frame_categorie.update_scrollbar()
+        selected_tab = self.notebook.select()
+        if selected_tab == ".!framemain.!notebook.!frame":
+            self.frame_motdepasse.update_scrollbar()
+        elif selected_tab == ".!framemain.!notebook.!frame2":
+            self.frame_categorie.update_scrollbar()
+        return
 
 
 
--- app/pages/app_frame/frame_motdepasse.py
+++ app/pages/app_frame/frame_motdepasse.py
@@ -17,5 +17,33 @@ class FrameMotdepasse(tk.Frame):
         frame_category = tk.Frame(self).pack(fill="x", pady=10)
         frame_motdepasse = tk.Frame(self).pack(fill="x", expand=True, pady=10)
 
-        
+    def update_scrollbar(self):
+        self.controller.update()
+        self.update_h_scroll()
+        self.update_v_scroll()
+
+
+    def update_v_scroll(self):
+        pass
+        # self.v_scroll.grid_forget()
+
+        # if not self.tree.get_children():
+        #     return
+
+        # for child in self.tree.get_children():
+        #     if not self.tree.bbox(child) or self.tree.bbox(child)[1]+self.tree.bbox(child)[3] > self.tree.winfo_height():
+        #         self.v_scroll.grid(row=0, column=1, sticky="ns")
+        #         return
+
+
+    def update_h_scroll(self):
+        pass
+        # self.h_scroll.grid_forget()
+        # width = 0
+        # for column in self.column: 
+        #     width += self.tree.column(column, "width")
+
+        # if width > self.tree.winfo_width() :
+        #     self.h_scroll.grid(row=1, column=0, sticky="ew")
+       
 
